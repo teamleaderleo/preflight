9d6c70482692dedd36c9b89b21ce21f328f368bc
Leo <cheerleaderleo@outlook.com>
2026-08-21 13:28:24 +0800

    Fit settled Speed without a 9px scrollbar (#562)

--- preflight-desktop/src/main.tsx
+++ preflight-desktop/src/main.tsx
@@ -6,6 +6,7 @@ import "./release-readiness.css";
 import "./game-settings-layout.css";
 import "./homePresentation.css";
 import "./layout-hierarchy.css";
+import "./speedPage.css";
 import { initializeHomePresentation } from "./useHomePresentation";
 
 initializeHomePresentation();
